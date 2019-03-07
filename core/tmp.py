068ad6b83a18e1968c9af3f5bd242be0e6ffc201
yaelahferr <45096288+yaelahferr@users.noreply.github.com>
2019-03-07 22:15:34 +0700

    Add files via upload

---
+++ core/tmp.py
@@ -0,0 +1,18 @@
+R = '\033[91m'
+G = '\033[92m'
+B = '\033[94m'
+Y = '\033[93m'
+W = '\033[0m'
+
+logo = f"""{B}
+
+                Check Generate Proxy {W}
+"""
+menu = f'''
+      {G}-1-{W} Search Proxy and Check
+      {G}-2-{W} Search Host
+      {G}-3-{W} Generate random Proxy
+      {G}-4-{W} Check Proxy in list file
+      {G}-5-{W} Check Single Proxy
+      {G}-0-{W} Exit
+        '''
